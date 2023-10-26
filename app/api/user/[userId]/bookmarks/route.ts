322badf6a5aa2ec55a4cbc51347fe32a73742175
Aaron Layton <aaron.r.layton@gmail.com>
2023-10-26 22:13:41 +0100

    cache test

--- app/api/user/[userId]/bookmarks/route.ts
+++ app/api/user/[userId]/bookmarks/route.ts
@@ -5,18 +5,18 @@ export const dynamic = 'force-static'
 export const revalidate = 'force-cache'
 
 export const GET = async () => {
-    // const team1Bookmarks = await getTeamBookmarks("1");
-    // const team2Bookmarks = await getTeamBookmarks("2");
-    // const team3Bookmarks = await getTeamBookmarks("3");
-    // const team4Bookmarks = await getTeamBookmarks("4");
+    const team1Bookmarks = await getTeamBookmarks("1");
+    const team2Bookmarks = await getTeamBookmarks("2");
+    const team3Bookmarks = await getTeamBookmarks("3");
+    const team4Bookmarks = await getTeamBookmarks("4");
 
     const currentTimestamp = new Date();
 
     return NextResponse.json({
         currentTimestamp,
-        // team1Bookmarks,
-        // team2Bookmarks,
-        // team3Bookmarks,
-        // team4Bookmarks
+        team1Bookmarks,
+        team2Bookmarks,
+        team3Bookmarks,
+        team4Bookmarks
     });
 }
