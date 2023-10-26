7bfd5df1f080aba17230cb0fad61c33fee0d7c10
Aaron Layton <aaron.r.layton@gmail.com>
2023-10-26 21:31:18 +0100

    fixing calling issue

--- app/utils/utils.ts
+++ app/utils/utils.ts
@@ -3,5 +3,5 @@ import { unstable_cache } from "next/cache";
 export const getTeamBookmarks = (teamId: string) => {
     return unstable_cache(async (teamId) => {
         return new Date();
-    }, [`team-bookmarks-${teamId}`]);
+    }, [`team-bookmarks-${teamId}`])(teamId);
 }
