86a5e1f7396f0b42599be21bc56c8fc604de9930
Aaron Layton <aaron.r.layton@gmail.com>
2023-10-26 21:27:57 +0100

    New test

---
+++ app/utils/utils.ts
@@ -0,0 +1,7 @@
+import { unstable_cache } from "next/cache";
+
+export const getTeamBookmarks = (teamId: string) => {
+    return unstable_cache(async (teamId) => {
+        return new Date();
+    }, [`team-bookmarks-${teamId}`]);
+}
